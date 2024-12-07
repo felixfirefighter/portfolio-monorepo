261cfec0249347dcd3431ea090d7d166216ae4ed
Felix Lee <leeyoongti@gmail.com>
2024-12-07 23:21:28 +0800

    feat: 🎸 add gfe-marketing

---
+++ apps/gfe-marketing/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@repo/design-system/components/ui/button';
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Hello</Button>
+    </div>
+  );
+}
--- packages/design-system/index.tsx
+++ packages/design-system/index.tsx
@@ -1,7 +1,3 @@
-import { AnalyticsProvider } from '@repo/analytics';
-import { AuthProvider } from '@repo/auth/provider';
-import { env } from '@repo/env';
-import { VercelToolbar } from '@vercel/toolbar/next';
 import type { ThemeProviderProps } from 'next-themes';
 import { Toaster } from './components/ui/sonner';
 import { TooltipProvider } from './components/ui/tooltip';
@@ -14,12 +10,12 @@ export const DesignSystemProvider = ({
   ...properties
 }: DesignSystemProviderProperties) => (
   <ThemeProvider {...properties}>
-    <AuthProvider>
-      <AnalyticsProvider>
-        <TooltipProvider>{children}</TooltipProvider>
-        <Toaster />
-        {env.NODE_ENV === 'development' && <VercelToolbar />}
-      </AnalyticsProvider>
-    </AuthProvider>
+    {/* <AuthProvider> */}
+    {/* <AnalyticsProvider> */}
+    <TooltipProvider>{children}</TooltipProvider>
+    <Toaster />
+    {/* {env.NODE_ENV === 'development' && <VercelToolbar />} */}
+    {/* </AnalyticsProvider> */}
+    {/* </AuthProvider> */}
   </ThemeProvider>
 );

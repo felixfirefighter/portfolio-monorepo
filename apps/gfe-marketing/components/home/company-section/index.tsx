3dc2bd614df438ab139a2a2885c33a74138e15e4
Felix Lee <leeyoongti@gmail.com>
2025-01-23 22:10:47 +0800

    feat: 🎸 marketing: finish up company marquee section

--- apps/gfe-marketing/components/home/company-section/index.tsx
+++ apps/gfe-marketing/components/home/company-section/index.tsx
@@ -1,40 +1,31 @@
 import type { Company } from '@/types/home/company';
 import Image from 'next/image';
+import Marquee from 'react-fast-marquee';
 
 const CompanySection = () => {
   const companies: Company[] = [
-    { name: 'PhotoStock Pro', logo: '/logo/logo-1.png' },
-    { name: 'DesignFlow', logo: '/logo/logo-1.png' },
-    { name: 'CreativeSpace', logo: '/logo/logo-1.png' },
-    { name: 'ArtisticHub', logo: '/logo/logo-1.png' },
-    { name: 'MediaVault', logo: '/logo/logo-1.png' },
-    { name: 'PixelPerfect', logo: '/logo/logo-1.png' },
-    { name: 'StudioLabs', logo: '/logo/logo-1.png' },
+    { name: 'Wan Nain', logo: '/logo/logo-1.png' },
+    { name: 'Robinwood', logo: '/logo/logo-2.png' },
+    { name: 'Swapdo', logo: '/logo/logo-3.png' },
+    { name: 'Diamond', logo: '/logo/logo-4.png' },
+    { name: 'Air Car', logo: '/logo/logo-5.png' },
+    { name: 'Makro Hard', logo: '/logo/logo-6.png' },
   ];
 
-  const renderLogos = (companies: Company[]) =>
-    companies.map((company) => (
-      <div
-        key={company.name}
-        className="mx-4 flex h-20 w-32 flex-shrink-0 flex-col items-center justify-center px-2 py-1"
-      >
-        <Image src={company.logo} alt={company.name} width={164} height={48} />
-      </div>
-    ));
-
   return (
-    <div className="w-full overflow-hidden">
-      <div className="relative flex">
-        {/* First set of logos */}
-        <div className="flex animate-marquee whitespace-nowrap">
-          {renderLogos(companies)}
-        </div>
-
-        {/* Duplicate set of logos for seamless loop */}
-        <div className="flex animate-marquee2 whitespace-nowrap">
-          {renderLogos(companies)}
-        </div>
-      </div>
+    <div>
+      <Marquee>
+        {companies.map((company) => (
+          <div key={company.name} className="px-10 py-5">
+            <Image
+              src={company.logo}
+              alt={company.name}
+              width={158}
+              height={38}
+            />
+          </div>
+        ))}
+      </Marquee>
     </div>
   );
 };

3535d496f92bee654ffe481a4c4006deeb77c613
Felix Lee <leeyoongti@gmail.com>
2025-03-02 21:03:25 +0800

    refactor: 💡 Unsplash interfaces

--- packages/api-unsplash/types/index.ts
+++ packages/api-unsplash/types/index.ts
@@ -1,16 +1,52 @@
 export interface UnsplashUser {
   id: string;
+  updated_at: string;
   username: string;
   name: string;
   first_name: string;
-  last_name: string | null;
+  last_name: string;
+  twitter_username: string | null;
+  portfolio_url: string | null;
+  bio: string;
+  location: string;
+  links: {
+    self: string;
+    html: string;
+    photos: string;
+    likes: string;
+    portfolio: string;
+    following: string;
+    followers: string;
+  };
   profile_image: {
     small: string;
     medium: string;
     large: string;
   };
-  bio: string | null;
-  location: string | null;
+  instagram_username: string | null;
+  total_collections: number;
+  total_likes: number;
+  total_photos: number;
+  total_promoted_photos: number;
+  total_illustrations: number;
+  total_promoted_illustrations: number;
+  accepted_tos: boolean;
+  for_hire: boolean;
+  social: {
+    instagram_username: string | null;
+    portfolio_url: string | null;
+    twitter_username: string | null;
+    paypal_email: string | null;
+  };
+}
+
+export interface UnsplashPhotoUrls {
+  raw: string;
+  full: string;
+  regular: string;
+  small: string;
+  thumb: string;
+  small_s3: string;
 }
 
 export interface UnsplashPhoto {
@@ -26,14 +62,7 @@ export interface UnsplashPhoto {
   blur_hash: string;
   description: string | null;
   alt_description: string;
-  urls: {
-    raw: string;
-    full: string;
-    regular: string;
-    small: string;
-    thumb: string;
-    small_s3: string;
-  };
+  urls: UnsplashPhotoUrls;
   links: {
     self: string;
     html: string;
@@ -43,47 +72,7 @@ export interface UnsplashPhoto {
   likes: number;
   liked_by_user: boolean;
   asset_type: string;
-  user: {
-    id: string;
-    updated_at: string;
-    username: string;
-    name: string;
-    first_name: string;
-    last_name: string;
-    twitter_username: string | null;
-    portfolio_url: string | null;
-    bio: string;
-    location: string;
-    links: {
-      self: string;
-      html: string;
-      photos: string;
-      likes: string;
-      portfolio: string;
-      following: string;
-      followers: string;
-    };
-    profile_image: {
-      small: string;
-      medium: string;
-      large: string;
-    };
-    instagram_username: string | null;
-    total_collections: number;
-    total_likes: number;
-    total_photos: number;
-    total_promoted_photos: number;
-    total_illustrations: number;
-    total_promoted_illustrations: number;
-    accepted_tos: boolean;
-    for_hire: boolean;
-    social: {
-      instagram_username: string | null;
-      portfolio_url: string | null;
-      twitter_username: string | null;
-      paypal_email: string | null;
-    };
-  };
+  user: UnsplashUser;
 }
 
 export interface UnsplashSearchResponse {

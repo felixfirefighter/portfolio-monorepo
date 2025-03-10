fc431c21bcca8105af2c9064a50a23172c0a997c
Felix Lee <leeyoongti@gmail.com>
2025-03-10 16:54:31 +0800

    feat: 🎸 make topic list section smaller on bigger screen

--- apps/gfe-pinsplash/features/shell/components/topics-list-section/index.tsx
+++ apps/gfe-pinsplash/features/shell/components/topics-list-section/index.tsx
@@ -2,7 +2,6 @@
 
 import { routes } from '@/features/shell/config/routes';
 import type { TopicRouteParams } from '@/features/shell/types/routes';
-import { getOptimalImageUrlForProfilePic } from '@/features/shell/utils/image';
 import { useGetTopicsQuery } from '@repo/api-unsplash';
 import { Button } from '@repo/design-system/components/ui/button';
 import {
@@ -47,14 +46,14 @@ export const TopicsListSection = () => {
         <div className="flex w-max space-x-4 px-8 pt-2 pb-4">
           {isLoading
             ? Array.from({ length: 20 }).map((_, i) => (
-                <Skeleton key={i} className="h-8 w-24 rounded-full md:h-12" />
+                <Skeleton key={i} className="h-8 w-24 rounded-full" />
               ))
             : topics?.map((topic) => (
                 <Button
                   variant={'ghost'}
                   key={topic.id}
                   className={cn(
-                    'flex h-10 cursor-pointer items-center gap-0 rounded-full bg-muted p-1 text-neutral-900 md:h-12',
+                    'flex h-10 cursor-pointer items-center gap-0 rounded-full bg-muted p-1 text-neutral-900',
                     {
                       'bg-primary/20 hover:bg-primary/20 focus:bg-primary/20 active:bg-primary/20':
                         params.slug === topic.slug,
@@ -72,14 +71,14 @@ export const TopicsListSection = () => {
                   <Image
                     src={topic.cover_photo.urls.thumb}
                     alt={topic.description}
-                    width={getOptimalImageUrlForProfilePic(size)}
-                    height={getOptimalImageUrlForProfilePic(size)}
-                    className="h-8 w-8 flex-shrink-0 rounded-full md:h-10 md:w-10"
+                    width={32}
+                    height={32}
+                    className="h-8 w-8 flex-shrink-0 rounded-full "
                     style={{
                       background: topic.cover_photo.color,
                     }}
                   />
-                  <p className="mx-2 flex-shrink-0 gap-0 font-medium text-sm md:mx-3 md:text-base">
+                  <p className="mx-2 flex-shrink-0 gap-0 font-medium text-sm md:mx-3">
                     {topic.title}
                   </p>
                 </Button>

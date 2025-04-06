ef56874d94080db0d9ad1d7794674b1b5c31c047
Felix Lee <leeyoongti@gmail.com>
2025-04-06 21:18:03 +0800

    feat: 🎸 add template for product details page

---
+++ apps/gfe-ecommerce/features/home/components/products-section-list/index.tsx
@@ -0,0 +1,77 @@
+import { routes } from '@/features/shell/config/routes';
+import { formatCentsToDollars } from '@/features/store/utils/format';
+import { useGetProductsQuery } from '@repo/api-ecommerce';
+import { Skeleton } from '@repo/design-system/components/ui/skeleton';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export const ProductsSectionList = () => {
+  const { data, isFetching } = useGetProductsQuery({});
+
+  if (isFetching || !data) {
+    return (
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-4">
+        {Array.from({ length: 8 }).map((_, index) => (
+          <div key={index} className="border-none shadow-none">
+            <Skeleton className="aspect-square w-full rounded-md" />
+            <Skeleton className="my-4" />
+            <Skeleton className="my-2 h-6 rounded-md" />
+
+            <Skeleton className="my-2 h-6 w-16 rounded-md" />
+            <Skeleton className="my-2 h-6 w-32 rounded-md" />
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  return (
+    <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-4">
+      {data.products.map((product) => {
+        return (
+          <Link
+            key={product.productId}
+            href={routes.products(product.productId)}
+          >
+            {product.imageUrl && (
+              <Image
+                src={product.imageUrl}
+                alt={product.name}
+                width={240}
+                height={240}
+                className="aspect-square h-auto w-full rounded-md object-cover"
+              />
+            )}
+            <div className="py-4">
+              <h3 className="mb-2 font-medium text-lg text-neutral-900 md:text-2xl">
+                {product.name}
+              </h3>
+              <div className="mb-4 flex items-center gap-2 ">
+                <span className="text-neutral-500 md:text-xl">
+                  {formatCentsToDollars(
+                    product.salePrice || product.listPrice || 0
+                  )}
+                </span>
+                {product.salePrice !== product.listPrice && (
+                  <span className="text-neutral-500 text-xs line-through md:text-lg">
+                    {formatCentsToDollars(product.listPrice || 0)}
+                  </span>
+                )}
+              </div>
+
+              <div className="flex gap-2">
+                {product.colors.map((color) => (
+                  <span
+                    key={color}
+                    className="h-4 w-4 rounded-full border border-neutral-300 md:h-5 md:w-5"
+                    style={{ backgroundColor: color }}
+                  />
+                ))}
+              </div>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
--- apps/gfe-ecommerce/features/home/components/products-section/index.tsx
+++ apps/gfe-ecommerce/features/home/components/products-section/index.tsx
@@ -1,35 +1,11 @@
 'use client';
 
-import { formatCentsToDollars } from '@/features/store/utils/format';
-import { useGetProductsQuery } from '@repo/api-ecommerce';
+import { ProductsSectionList } from '@/features/home/components/products-section-list';
 import { Button } from '@repo/design-system/components/ui/button';
-import { Card, CardContent } from '@repo/design-system/components/ui/card';
-import { Skeleton } from '@repo/design-system/components/ui/skeleton';
-import Image from 'next/image';
+import {} from '@repo/design-system/components/ui/card';
 import Link from 'next/link';
 
 export const ProductsSection = () => {
-  const { data, isFetching } = useGetProductsQuery({});
-
-  if (isFetching || !data) {
-    return (
-      <div className="container grid grid-cols-1 gap-8 py-10 md:grid-cols-2 lg:py-20 xl:grid-cols-4">
-        {Array.from({ length: 8 }).map((_, index) => (
-          <Card key={index} className="border-none shadow-none">
-            <CardContent className="p-0">
-              <Skeleton className="aspect-square w-full rounded-md" />
-              <Skeleton className="my-4" />
-              <Skeleton className="my-2 h-6 rounded-md" />
-
-              <Skeleton className="my-2 h-6 w-16 rounded-md" />
-              <Skeleton className="my-2 h-6 w-32 rounded-md" />
-            </CardContent>
-          </Card>
-        ))}
-      </div>
-    );
-  }
-
   return (
     <section className="container py-10 lg:py-20">
       <div className="mb-6 flex items-center justify-between lg:mb-8">
@@ -42,52 +18,7 @@ export const ProductsSection = () => {
         </Link>
       </div>
 
-      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-4">
-        {data.products.map((product) => {
-          return (
-            <Card key={product.productId} className="border-none shadow-none">
-              <CardContent className="p-0">
-                {product.imageUrl && (
-                  <Image
-                    src={product.imageUrl}
-                    alt={product.name}
-                    width={240}
-                    height={240}
-                    className="aspect-square h-auto w-full rounded-md object-cover"
-                  />
-                )}
-                <div className="py-4">
-                  <h3 className="mb-2 font-medium text-lg text-neutral-900 md:text-2xl">
-                    {product.name}
-                  </h3>
-                  <div className="mb-4 flex items-center gap-2 ">
-                    <span className="text-neutral-500 md:text-xl">
-                      {formatCentsToDollars(
-                        product.salePrice || product.listPrice || 0
-                      )}
-                    </span>
-                    {product.salePrice !== product.listPrice && (
-                      <span className="text-neutral-500 text-xs line-through md:text-lg">
-                        {formatCentsToDollars(product.listPrice || 0)}
-                      </span>
-                    )}
-                  </div>
-
-                  <div className="flex gap-2">
-                    {product.colors.map((color) => (
-                      <span
-                        key={color}
-                        className="h-4 w-4 rounded-full border border-neutral-300 md:h-5 md:w-5"
-                        style={{ backgroundColor: color }}
-                      />
-                    ))}
-                  </div>
-                </div>
-              </CardContent>
-            </Card>
-          );
-        })}
-      </div>
+      <ProductsSectionList />
     </section>
   );
 };
